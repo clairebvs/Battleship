4f791ac769cdfe13e8f29c21f3e7a024d6e830a0
ClaireBvs <clairembeauvais@gmail.com>
2018-05-19 14:22:03 -0600

    implements method grid computer and grid player as array and test

--- lib/grid.rb
+++ lib/grid.rb
@@ -2,8 +2,8 @@ require 'terminal-table.rb'
 class Grid
 
   def initialize
-    @grid_player = {}
-    @grid_computer = Hash.new
+    @player_grid = []
+    @computer_grid = Array.new
   end
 
   def grid_shape
@@ -16,22 +16,29 @@ class Grid
     table = Terminal::Table.new :rows => rows
 
     table.style = {:width => 40, :padding_left => 3, :border_x => "=" }
-   end
-
-  def grid_player_hash
-    @grid[:A] << [1, 2, 3, 4]
-    @grid[:B] << [1, 2, 3, 4]
-    @grid[:C] << [1, 2, 3, 4]
-    @grid[:D] << [1, 2, 3, 4]
   end
 
-  def grid_computer_hash
-    @grid[:A] << [1, 2, 3, 4]
-    @grid[:B] << [1, 2, 3, 4]
-    @grid[:C] << [1, 2, 3, 4]
-    @grid[:D] << [1, 2, 3, 4]
+  def grid_player_array
+    row_a = [".", ".", ".", "."]
+    row_b = [".", ".", ".", "."]
+    row_c = [".", ".", ".", "."]
+    row_d = [".", ".", ".", "."]
+    @player_grid << row_a
+    @player_grid << row_b
+    @player_grid << row_c
+    @player_grid << row_d
+  end
+
+  def grid_computer_array
+    row_a = [".", ".", ".", "."]
+    row_b = [".", ".", ".", "."]
+    row_c = [".", ".", ".", "."]
+    row_d = [".", ".", ".", "."]
+    @computer_grid << row_a
+    @computer_grid << row_b
+    @computer_grid << row_c
+    @computer_grid << row_d
   end
 end
 
-puts table
-# :border_i => "x"
+# puts table
---
+++ test/grid_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/grid'
+
+class GridTest < Minitest::Test
+
+  def test_it_exists
+    grid = Grid.new
+    assert_instance_of Grid, grid
+  end
+
+  def test_it_has_a_grid_player
+    grid = Grid.new
+    assert_equal [[".", ".", ".", "."], [".", ".", ".", "."], [".", ".", ".", "."], [".", ".", ".", "."]], grid.grid_player_array
+  end
+
+  def test_it_has_a_grid_player
+    grid = Grid.new
+    assert_equal [[".", ".", ".", "."], [".", ".", ".", "."], [".", ".", ".", "."], [".", ".", ".", "."]], grid.grid_computer_array
+  end
+end
+=begin
+1 ) method to remember previous shots in array and iterate through to see if it contains already same user input and then store user_input or computer here
+2 ) has to know about class ship
+3 ) update grid after round
+=end
