8b78dfb6dd1beb41ce04684dae22de006aa154d7
ClaireBvs <clairembeauvais@gmail.com>
2018-05-21 10:57:38 -0600

    move method place ship to grid class

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,10 +1,9 @@
 class Ship
+
   attr_reader :length,
               :sunk
 
   def initialize
-    @computer_ships = Hash.new
-    @player_ships = Hash.new
     @length = length
     @start_space = nil
     @end_space = nil
@@ -12,34 +11,19 @@ class Ship
     @touched_by_hit = 0
   end
 
-  def computer_ships
-    @computer_ships = {destroyer: 2, cruiser: 3}
-  end
-
-  def player_ships
-    @player_ships = {destroyer: 2, cruiser: 3}
-  end
-
   def sunk?
-    @sunk = true
+    # @sunk = true
     if @length == @touched_by_hit
       @sunk = true
     end
   end
 
-  def generate_computer_ships_position
-    destroyer = @grid_computer.map {[*'A'..'D', *'1'..'4'].sample}.join
-    # if @cell == 'water'
-
-    # boat_1 need to add another cells next to it
-
-    # +adding position goes right if ok do it if not goes down if not hoes left if not goes up
-    cruiser = @grid_computer.map {[*'A'..'D', *'1'..'4'].sample}.join
-    # horizontally or vertically
-    # random start cell
-    # one row and one column
+  def hit
+    @touched_by_hit += 1
   end
+
+  def length_ship
+    @length = @start_space + @end_space
+  end
+
 end
-
-
-# knows about the ship ... their length and positions
