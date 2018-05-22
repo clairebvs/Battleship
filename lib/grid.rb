94a9bd1ae737eb596be83c9ea4b8e795760171fc
ClaireBvs <clairembeauvais@gmail.com>
2018-05-21 20:39:54 -0600

    implements function place computer ships

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,80 +1,73 @@
 require 'terminal-table.rb'
 class Grid
+   attr_reader :grid_layout,
+               :position
 
   def initialize
-    @player_grid = []
-    @computer_grid = Array.new
-    @length = length
-    @coordinates = coordinates
+     # @grid_layout =
+     # [{"A1" => Square.new}, {"A2" => Square.new}, {"A3" => Square.new}, {"A4" => Square.new},
+     #  {"B1" => Square.new}, {"B2" => Square.new}, {"B3" => Square.new}, {"B4" => Square.new},
+     #  {"C1" => Square.new}, {"C2" => Square.new}, {"C3" => Square.new}, {"C4" => Square.new},
+     #  {"D1" => Square.new}, {"D2" => Square.new}, {"D3" => Square.new}, {"D4" => Square.new}]
+    @grid_layout =
+    [{"A1" => Square.new, "A2" => Square.new, "A3" => Square.new, "A4" => Square.new,
+     "B1" => Square.new, "B2" => Square.new, "B3" => Square.new, "B4" => Square.new,
+     "C1" => Square.new, "C2" => Square.new, "C3" => Square.new, "C4" => Square.new,
+     "D1" => Square.new, "D2" => Square.new, "D3" => Square.new, "D4" => Square.new}]
+    @position = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    @ship_location = []
   end
 
-  def grid_layout
-    cells = [[]]
+  def coordinates(x, y)
+    @coordinates = [x, y]
   end
 
-  def grid_shape
-    rows = []
-    rows << [".", 1, 2 ,3, 4]
-    rows << ["A", " ", " ", " ", " "]
-    rows << ["B"]
-    rows << ["C"]
-    rows << ["D"]
-    table = Terminal::Table.new :rows => rows
-
-    table.style = {:width => 40, :padding_left => 3, :border_x => "=" }
+  def find_first_random_position_computer_ship
+    first_position = @position.sample
+    array_first_position = first_position.split(//)
+    return array_first_position
   end
 
-  def grid_player_array
-    x = "."
-    x = Square.new
-    grid_layout = [[".", ".", ".", "."],
-            [".", ".", ".", "."],
-            [".", ".", ".", "."],
-            [".", ".", ".", "."]]
-    @player_grid << grid
-  end
-
-  def grid_computer_array
-    row_a = [".", ".", ".", "."]
-    row_b = [".", ".", ".", "."]
-    row_c = [".", ".", ".", "."]
-    row_d = [".", ".", ".", "."]
-    @computer_grid << row_a
-    @computer_grid << row_b
-    @computer_grid << row_c
-    @computer_grid << row_d
-  end
-
-  def place_computer_ship(length)
-      row = [*'A'..'D']
-      column = [*'1'..'4']
-      first_square = @grid_computer.map {[*'A'..'D', *'1'..'4'].sample}.join
-      array_strings = first_square.split(//)
-
-      @coordinates = (x, y)
-      x = array_strings[1]
-      y = array_strings[0]
-      hor_ver = rand(2)
-      if hor_ver == 0
-        # horizontally
-          x+1 || x-1
-      elsif hor_ver == 1
-        # vertical
-          y+1 || y-1
-      end
-
-      if
-        # going right
-
-      # boat_1 need to add another cells next to it
-
-      # +adding position goes right if ok do it if not goes down if not hoes left if not goes up
-
-      # horizontally or vertically
-      # random start cell
-      # one row and one column
+  def place_computer_ship(ship_length, x = 0, y = 0)
+    first_po = find_first_random_position_computer_ship
+    x = first_po[1]
+    y = first_po[0]
+    # ship_length_min_1 = ship_length - 1
+    # second_position = []
+    # ship_length_min_1.times do
+    if @position.include?((y.ord + 1).chr)
+      so = (y.ord + 1).chr
+    else
+      so = (y.ord - 1).chr
     end
+    y = so
+    x = first_po[1]
+    # require 'pry' ;binding.pry
+    second = (y + x).chars.join
+    @ship_location << first_po.join
+    @ship_location << second
+    return @ship_location
+
+
+
+
+    # if @position.include?((x.ord + 1).chr)
+    #   (x.ord + 1).chr
+    # else
+    #   (x.ord - 1).chr
+    # end
+  end
+
+      # first_square = @grid_layout.map do |element|
+      #   element.sample
+      # end
+      # first_element_array = first_square.shift
+      # @ship_location << first_element_array
+      # array_strings = first_element_array.split(//)
+      # x = array_strings[1]
+      # y = array_strings[0]
+      # @coordinates = ([x, y])
+      # @ship_location.push([x, y])
+    # end
 
 end
-
-# puts table
--- test/grid_test.rb
+++ test/grid_test.rb
@@ -1,7 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/grid'
-
+require './lib/square'
+require 'pry'
 class GridTest < Minitest::Test
 
   def test_it_exists
@@ -9,18 +10,33 @@ class GridTest < Minitest::Test
     assert_instance_of Grid, grid
   end
 
-  def test_it_has_a_grid_player
+  def test_it_has_a_grid_to_play
     grid = Grid.new
-    assert_equal [[".", ".", ".", "."], [".", ".", ".", "."], [".", ".", ".", "."], [".", ".", ".", "."]], grid.grid_player_array
+    assert_equal  ["A1", "A2", "A3", "A4",
+                   "B1", "B2", "B3", "B4",
+                   "C1", "C2", "C3", "C4",
+                   "D1", "D2", "D3", "D4"], grid.grid_layout[0].keys
   end
 
-  def test_it_has_a_grid_player
+  def test_it_has_a_collection_of_position
     grid = Grid.new
-    assert_equal [[".", ".", ".", "."], [".", ".", ".", "."], [".", ".", ".", "."], [".", ".", ".", "."]], grid.grid_computer_array
+    assert grid.position.include?("A2")
+    assert grid.position.include?("D2")
   end
 
-  
+  def test_can_find_first_random_square
+    grid = Grid.new
 
+    assert_instance_of Array, grid.find_first_random_position_computer_ship
+    assert_equal 2, grid.find_first_random_position_computer_ship.length
+  end
+  def test_place_computer_ship
+    grid = Grid.new
+    x = 2
+    y = "A"
+
+    assert_equal ["A2", "A3"], grid.place_computer_ship(2, x, y)
+  end
 end
 =begin
 1 ) method to remember previous shots in array and iterate through to see if it contains already same user input and then store user_input or computer here
